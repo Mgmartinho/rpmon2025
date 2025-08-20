b36411f3c2dc1a22eeb0f20c22875c6c32fcb60f
Marcelo Martinho <martinho.guilherme13@hotmail.com>
2025-08-20 09:05:17 -0300

    cmt

--- src/Pages/ComandanteRpmon/index.js
+++ src/Pages/ComandanteRpmon/index.js
@@ -14,7 +14,7 @@ const ComandantesRpmon = () => {
     </h2>
     <div class="row">
       <div class="col-md-5 col-12 mb-4">
-        <h3 class="text-dark">Ten. Cel. André Frisene</h3>
+        <h3 class="text-dark">Ten. Cel. André Frisenes</h3>
         <a href='/comandante'>
           <img src={CelDaCruz} alt="Foto do Comandante" class="pt-1 w-100 fixed-image rounded-3" />
         </a>
